7919642a2f100d6205e1b941b88d63471fbeba0f
Saravanan <saravanan14.4.2005@gmail.com>
2025-02-17 23:46:28 +0530

    mod app/init-comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
-
+import Comp from './Comp'
 function App() {
-  alert("My name is saravanan");
-  function yaro() {
-    console.log("hiiiiiiiiii");
-  }
-
-  const bhaii =()=>{
-  console.log("hellloooooooo");
-  }
+ 
   const name={
     name:"saravanan",
     age:20,
   };
+
+
   console.log("age="+name.age);
+
+  
   return (
     
-    <div className='screen'>
-    <h1>name:{name.name}</h1>
-    <button onClick={yaro}>presss me</button>
-    <button onClick={()=>console.log("....CONGRAJULATION .....")}> dont click</button>
-    <button onClick={bhaii}> touch </button>  
+    <div>  
+      <h1>name:{name.name}</h1>
+      <div id='button'>
+        <button onClick={()=>alert("click is clicked")}>click</button>
+        <button onDoubleClick={()=>alert("2click is clicked")}>2clicked</button>
+      </div>
+       
+       
+      <div id='container'>
+        <Comp  name="saravanan" Age="20" FriendName="vicky" FAge="20"/>
+      </div>
+    
+   
      </div> 
   );
 }
